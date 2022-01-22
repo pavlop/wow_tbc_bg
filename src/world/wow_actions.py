aa1999da99f7b4cde9c6ae31581bf7f787db7eb7
pavlop <poliakov.pavel@gmail.com>
2022-01-22 15:27:13 -0800

    added some pictures

--- src/world/wow_actions.py
+++ src/world/wow_actions.py
@@ -4,13 +4,14 @@ import pyautogui
 
 
 def run(time_sec):
-  half_time = time_sec/2.0
+  half_time = time_sec / 2.0
   pyautogui.keyDown('w')
   time.sleep(half_time)
   pyautogui.keyDown('space')
   pyautogui.keyUp('space')
   time.sleep(half_time)
   pyautogui.keyUp('w')
+
 
 def strafe(time_sec, left_or_right):
   if left_or_right == 'right':
@@ -24,22 +25,22 @@ def strafe(time_sec, left_or_right):
   else:
     print('Wrong direction for strafe', left_or_right)
 
+
 def turn(left_or_right):
   pyautogui.keyDown(left_or_right)
   pyautogui.keyUp(left_or_right)
 
+
 def run_and_turn_in_the_middle(time_sec, left_or_right):
   pyautogui.keyDown('w')
-  time.sleep(time_sec/2.0)
+  time.sleep(time_sec / 2.0)
   turn(left_or_right)
-  pyautogui.keyDown('space')
-  pyautogui.keyUp('space')
-  time.sleep(time_sec/2.0)
+  time.sleep(time_sec / 2.0)
   pyautogui.keyUp('w')
 
+
 def run_and_strafe_in_the_middle(time_run_sec, left_or_right, time_strafe_sec):
-  pyautogui.keyDown('w')
-  time.sleep(time_run_sec/2.0)
+  time_half_sec = time_run_sec / 2.0
+  run(time_half_sec)
   strafe(time_strafe_sec, left_or_right)
-  time.sleep(time_run_sec/2.0)
-  pyautogui.keyUp('w')
+  run(time_half_sec)
